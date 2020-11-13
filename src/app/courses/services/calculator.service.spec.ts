0bd269e1a0b07a0e3c2c7f05cfb34214892a64c0
Will Reichl <William.Reichl@dcsg.com>
2020-11-13 08:05:50 -0500

    S1L5 - First sample Jasmine test skeleton

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,10 @@
+describe('CalculatorService', () => {
+  // "specification", i.e. "test"
+  it('should add two numbers', () => {
+    pending(); // lets javascript know this test is not ready to be run yet
+  });
+
+  it('should add subtract numbers', () => {
+    fail();
+  });
+});

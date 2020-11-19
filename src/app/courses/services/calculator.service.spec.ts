0c962a5bde5ffea37abeb711cdd5488e8e92b252
Will Reichl <willreichl@gmail.com>
2020-11-19 06:53:01 -0500

    S1 - First Jasmine test implementation

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,10 +1,21 @@
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
 describe('CalculatorService', () => {
   // "specification", i.e. "test"
   it('should add two numbers', () => {
-    pending(); // lets javascript know this test is not ready to be run yet
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4, 'Unexpected addition result');
   });
 
-  it('should add subtract numbers', () => {
-    fail();
+  it('should subtract numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, 'Unexpected subtraction result');
   });
 });

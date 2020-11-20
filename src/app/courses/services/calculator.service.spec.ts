0e85008a8e2b16b51abc00ced5faa8207548eda9
Will Reichl <willreichl@gmail.com>
2020-11-20 06:44:40 -0500

    S1 - The Angular Test Bed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
@@ -7,7 +8,15 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService);
   });
 
   // "specification", i.e. "test"

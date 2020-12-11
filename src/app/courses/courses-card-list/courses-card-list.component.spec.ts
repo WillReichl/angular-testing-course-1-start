800ac41f20e276e8638d65551460a4bed575dc5e
Will Reichl <willreichl@gmail.com>
2020-12-11 10:05:06 -0500

    S3 - Test setup w/ async test utility * Async allows all async setup steps to finish before each step * Kind of like an "AwaitAll"

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -9,14 +9,25 @@ import { Course } from '../model/course';
 import { setupCourses } from '../common/setup-test-data';
 
 describe('CoursesCardListComponent', () => {
-  beforeEach(() => {
+  let component: CoursesCardListComponent; // each test gets a fresh component
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  // async here is not a JS language feature, specific to Angular testing
+  // Basically we wait until all async operations in this block complete before starting each test.
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule],
-    });
-  });
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
 
   it('should create the component', () => {
-    pending();
+    expect(component).toBeTruthy();
+    // console.log(component);
   });
 
   it('should display the course list', () => {

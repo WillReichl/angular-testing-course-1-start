43fc31ced79f1a7cf9bc5220410dfd7ed838d751
Will Reichl <willreichl@gmail.com>
2021-01-05 08:23:54 -0500

    24. Container Component Test Suite - The Home Component

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -33,6 +33,10 @@ describe('HomeComponent', () => {
     (c) => c.category === 'BEGINNER'
   );
 
+  const advancedCourses = setupCourses().filter(
+    (c) => c.category === 'ADVANCED'
+  );
+
   beforeEach(async(() => {
     // define mock service
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', [
@@ -68,11 +72,24 @@ describe('HomeComponent', () => {
   });
 
   it('should display only advanced courses', () => {
-    pending();
+    // must return observable of courses, not array of courses, hence rxjs "of"
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
   });
 
   it('should display both tabs', () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    expect(tabs.length).toBe(2, 'Unexpected number of tabs found');
   });
 
   it('should display advanced courses when tab clicked', () => {

dc7b42a3365cd45a950724153c505b86ce7fd1f4
Will Reichl <willreichl@gmail.com>
2021-03-07 22:23:47 -0500

    S5 - Simulating user interaction in end-to-end test

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,23 +1,40 @@
 // Turn on cypress intellisense
 /// <reference types="Cypress" />
 
+beforeEach(() => {
+  // create a fixture and give the simulated HTTP response a name
+  cy.fixture('courses.json').as('coursesJSON');
+
+  // start the mock cypress back end
+  cy.server();
+
+  // define the route to mock
+  cy.route('/api/courses', '@coursesJSON').as('courses');
+  cy.visit('/');
+});
+
 describe('Home Page', () => {
   it('should display a list of courses', () => {
-    // create a fixture and give the simulated HTTP response a name
-    cy.fixture('courses.json').as('coursesJSON');
-
-    // start the mock cypress back end
-    cy.server();
-
-    // define the route to mock
-    cy.route('/api/courses', '@coursesJSON').as('courses');
-
     // Act
-    cy.visit('/');
-    cy.contains('All Courses');
     cy.wait('@courses'); // wait for courses event to finish
 
     // Assert
+    cy.contains('All Courses');
     cy.get('mat-card').should('have.length', 9);
+  });
+
+  it('should display advanced courses on click', () => {
+    cy.get('.mat-tab-label').should('have.length', 2);
+    cy.get('.mat-tab-label').last().click();
+
+    // Cypress will handle the asyncronous nature of the click/change tab animation for us.
+
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .its('length')
+      .should('be.gt', 1);
+
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .first()
+      .should('contain', 'Angular Security Course');
   });
 });
